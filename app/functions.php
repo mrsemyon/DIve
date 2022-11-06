9511a4fb076247192b6c8f7b4a10f0434ff229b8
mrSemyon <syoma4k@gmail.com>
2022-11-07 03:32:50 +0500

    Added getUserByEmail() function

--- app/functions.php
+++ app/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function makePDOConnection($config): PDO
+function makePDOConnection(array $config): PDO
 {
     return new PDO(
         "mysql:host={$config['db']['host']};dbname={$config['db']['name']};charset={$config['db']['charset']}",
@@ -8,4 +8,12 @@ function makePDOConnection($config): PDO
         $config['db']['password'],
         $config['db']['opt']
     );
+}
+
+function getUserByEmail(PDO $pdo, $email): array
+{
+    $sql = 'SELECT * FROM `users` WHERE `email` = :email';
+    $statement = $pdo->prepare($sql);
+    $statement->execute(['email' => $email]);
+    return $statement->fetchAll();
 }
