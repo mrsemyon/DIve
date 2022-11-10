179ba83c069f9ac5c7ce4d075b422509883f2854
mrSemyon <syoma4k@gmail.com>
2022-11-10 15:15:08 +0500

    Changed links

--- public/users.php
+++ public/users.php
@@ -60,7 +60,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/public/templates/header.php';
                             <span class="rounded-circle profile-image d-block " style="background-image:url('/upload/<?= $user['photo'] ?>'); background-size: cover;"></span>
                         </span>
                         <div class="info-card-text flex-1">
-                            <a href="/public/profile/?id=<?= $user['id'] ?>"><?= $user['name'] ?></a>
+                            <a href="/public/profile.php?id=<?= $user['id'] ?>"><?= $user['name'] ?></a>
                             <a href="javascript:void(0);" class="fs-xl text-truncate text-truncate-lg text-info" data-toggle="dropdown" aria-expanded="false">
                                 <?php if ($_SESSION['role'] == 'admin' || $user['email'] == $_SESSION['email']) { ?>
                                     <i class="fal fas fa-cog fa-fw d-inline-block ml-1 fs-md"></i>
