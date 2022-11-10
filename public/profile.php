0bc40355cc20d9c8d9e7ef5049bdc52bc647b711
mrSemyon <syoma4k@gmail.com>
2022-11-10 15:09:47 +0500

    Added rights checking control

--- public/profile.php
+++ public/profile.php
@@ -1,4 +1,12 @@
 <?php
+require $_SERVER['DOCUMENT_ROOT'] . '/app/core.php';
+
+if (!isset($_SESSION['email'])) {
+    setFlashMessage('danger', 'Необходима авторизация.');
+    redirect("/public/authorization.php");
+    exit;
+}
+
 require $_SERVER['DOCUMENT_ROOT'] . '/public/templates/header.php';
 ?>
 <div class="subheader">
