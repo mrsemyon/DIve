07ffe0c467f319680ecd6bc0116f4cffd99ff4de
mrSemyon <syoma4k@gmail.com>
2022-11-14 01:04:04 +0500

    Added delete controller

--- controllers/delete.php
+++ controllers/delete.php
@@ -8,3 +8,20 @@ if (($_SESSION['role'] != 'admin') && ($_SESSION['email'] != $user['email'])) {
     redirect("/public/users.php");
     exit;
 }
+
+deleteUser($pdo, $_GET['id']);
+
+if ($user['photo'] != 'no_photo.jpg') {
+    unlink($_SERVER['DOCUMENT_ROOT'] . '/upload/' . $user['photo']);
+}
+
+setFlashMessage('success', 'Пользователь успешно удалён.');
+
+if ($user['email'] == $_SESSION['email']) {
+    session_destroy();
+    redirect("/public/authorization.php");
+    exit;
+}
+
+redirect("/public/users.php");
+exit;
