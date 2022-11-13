2c2e747a71008c32fecac4acd4424fd74e66c16d
mrSemyon <syoma4k@gmail.com>
2022-11-14 00:52:04 +0500

    Added right checking control

--- controllers/delete.php
+++ controllers/delete.php
@@ -1,1 +1,10 @@
 <?php
+require $_SERVER['DOCUMENT_ROOT'] . '/app/core.php';
+
+$user = getUserById($pdo, $_GET['id']);
+
+if (($_SESSION['role'] != 'admin') && ($_SESSION['email'] != $user['email'])) {
+    setFlashMessage('danger', 'У Вас недостаточно прав');
+    redirect("/public/users.php");
+    exit;
+}
