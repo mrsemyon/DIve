73f8b345eaabd2c3f862e6d36527b3cb9641830c
mrSemyon <syoma4k@gmail.com>
2022-11-13 23:39:05 +0500

    Added controllers for set status

---
+++ controllers/status.php
@@ -0,0 +1,16 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/app/core.php';
+
+$user = getUserById($pdo, $_GET['id']);
+
+if (($_SESSION['role'] != 'admin') && ($_SESSION['email'] != $user['email'])) {
+    setFlashMessage('danger', 'У Вас недостаточно прав.');
+    redirect("/public/users.php");
+    exit;
+}
+
+setUserStatus($pdo, $_GET['id'], $_POST['status']);
+
+setFlashMessage('success', 'Информация успешно обновлена.');
+redirect("/public/users.php");
+exit;
