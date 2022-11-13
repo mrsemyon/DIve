0d7c93eefd3924ac5a78acd38a2c0e1d4c18c9be
mrSemyon <syoma4k@gmail.com>
2022-11-13 23:59:06 +0500

    Added rights checking control

--- public/media.php
+++ public/media.php
@@ -1,5 +1,16 @@
 <?php
+require $_SERVER['DOCUMENT_ROOT'] . '/app/core.php';
+
+$user = getUserById($pdo, $_GET['id']);
+
+if (($_SESSION['role'] != 'admin') && ($_SESSION['email'] != $user['email'])) {
+    setFlashMessage('danger', 'У Вас недостаточно прав.');
+    redirect("/public/users.php");
+    exit;
+}
+
 $title = 'Изменить аватар';
+
 require $_SERVER['DOCUMENT_ROOT'] . '/public/templates/header.php';
 ?>
         <div class="subheader">
